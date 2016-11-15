19abed3b3683577c98876e4505ff5b6ef5e9dbc8
makerbro <makerbro@acrobotic.com>
2016-11-14 19:40:31 -0800

    updating

---
+++ README.md
@@ -0,0 +1,20 @@
+# Ai_Tips_ESP8266
+
+Example code for using the ESP8266 Development Board with various electronic and
+mechanical components, as shown in our YouTube tutorial series "ESP8266 Tips &
+Tricks": 
+
+    * https://www.youtube.com/playlist?list=PLNFq0T6Z3JPsHwzvPQncip-kMIdWpnnip.
+
+Please consider buying products from us to help fund future Open-Source projects
+like this! We'll always put our best effort in every project, and release all
+our design files and code for you to use. 
+
+Visit our online shop, which is located at:
+
+   * https://acrobotic.com/
+
+## License
+
+Released under the MIT license. Please check LICENSE.txt for more information. 
+All text above must be included in any redistribution.
---
+++ simple_webserver/simple_webserver.ino
@@ -0,0 +1,37 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+
+ESP8266WebServer server;
+uint8_t pin_led = 16;
+char* ssid = "AcroboticGuest";
+char* password = "13meetup37";
+
+void setup()
+{
+  pinMode(pin_led, OUTPUT);
+  WiFi.begin(ssid,password);
+  Serial.begin(115200);
+  while(WiFi.status()!=WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println("");
+  Serial.print("IP Address: ");
+  Serial.print(WiFi.localIP());
+
+  server.on("/",[](){server.send(200,"text/plain","Hello World!");});
+  server.on("/toggle",toggleLED);
+  server.begin();
+}
+
+void loop()
+{
+  server.handleClient();
+}
+
+void toggleLED()
+{
+  digitalWrite(pin_led,!digitalRead(pin_led));
+  server.send(204,"");
+}
